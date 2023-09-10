bbc4acffea411f4684bf8009db35abc4c09a2282
ayman212212 <144522208+ayman212212@users.noreply.github.com>
2023-09-10 19:06:09 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,15 @@
 import streamlit as st
+import pandas as pd
 
-st.write("this is my site")
+# Sample DataFrame (Replace this with your own DataFrame)
+data = {
+    'Name': ['John', 'Alice', 'Bob', 'Eve', 'Charlie'],
+    'Age': [25, 30, 22, 28, 35],
+    'City': ['New York', 'Los Angeles', 'Chicago', 'Miami', 'San Francisco']
+}
+
+df = pd.DataFrame(data)
+
+# Display the first 5 records in a table
+st.write("First 5 Records of the DataFrame:")
+st.table(df.head(5))
