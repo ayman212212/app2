61b776660b45d64ff77cd40b2f354c7987ecd2d8
ayman212212 <144522208+ayman212212@users.noreply.github.com>
2023-09-10 19:36:36 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,13 +2,9 @@ import streamlit as st
 import pandas as pd
 
 # Sample DataFrame (Replace this with your own DataFrame)
-data = {
-    'Name': ['John', 'Alice', 'Bob', 'Eve', 'Charlie'],
-    'Age': [25, 30, 22, 28, 35],
-    'City': ['New York', 'Los Angeles', 'Chicago', 'Miami', 'San Francisco']
-}
 
-df = pd.DataFrame(data)
+
+df = pd.read_csv('CVS.csv')
 
 # Display the first 5 records in a table
 st.write("First 5 Records of the DataFrame:")
